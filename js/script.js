2b9e24b234db4ee991a0a0c9c2084a042181285d
William <william.teletabs@hotmail.com>
2022-10-22 16:10:50 -0300

    Importando nova função

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initScrollSuave from './modules/scroll-suave.js';
 import initFetchBtc from './modules/fetch-btc.js';
+import initAnimaScroll from './modules/anima-scroll.js';
 
 initScrollSuave();
 initFetchBtc();
+initAnimaScroll();
