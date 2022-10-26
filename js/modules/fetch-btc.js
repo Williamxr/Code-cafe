8e236a5b4c786b78e790864d6c307bb950e1cb1d
William <william.teletabs@hotmail.com>
2022-10-26 09:56:55 -0300

    Alterando número de casas decimais que aparecem para o usuário

--- js/modules/fetch-btc.js
+++ js/modules/fetch-btc.js
@@ -1,6 +1,6 @@
 export default function initFetchBtc() {
   fetch('https://blockchain.info/ticker').then((response) => response.json()).then((btc) => {
     const btcPreco = document.querySelector('.btc-preco');
-    btcPreco.innerText = (1000 / btc.BRL.sell).toFixed(3);
+    btcPreco.innerText = (1000 / btc.BRL.sell).toFixed(2);
   });
 }
