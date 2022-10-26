22cc2e961c9551129a810257e2da9a61e74d3ba4
William <william.teletabs@hotmail.com>
2022-10-26 16:11:22 -0300

    Importando novos arquivos js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import initScrollSuave from './modules/scroll-suave.js';
 import initFetchBtc from './modules/fetch-btc.js';
 import initAnimaScroll from './modules/anima-scroll.js';
+import initFuncionamento from './modules/funcionamento.js';
 
 initScrollSuave();
 initFetchBtc();
 initAnimaScroll();
+initFuncionamento();
