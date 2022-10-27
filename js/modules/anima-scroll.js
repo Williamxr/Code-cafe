53dd8212b66de38f88974d65fe72cc3854adf2d4
William <william.teletabs@hotmail.com>
2022-10-27 18:05:46 -0300

    Estilizando/configurando o projeto conforme eslint

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -1,21 +1,20 @@
-export default function initAnimaScroll(){
+export default function initAnimaScroll() {
   const sections = document.querySelectorAll('[data-tab="anima-scroll"]');
   const dist = window.innerHeight * 0.7;
 
-  if(sections.length){
-
-    function animaScroll(){
+  function animaScroll() {
+    if (sections.length) {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - dist) < 0
-        if(isSectionVisible){
+        const isSectionVisible = (sectionTop - dist) < 0;
+        if (isSectionVisible) {
           section.classList.add('anima');
-        }else{
+        } else {
           section.classList.remove('anima');
         }
-      })
+      });
     }
-    
     window.addEventListener('scroll', animaScroll);
   }
+  animaScroll();
 }
