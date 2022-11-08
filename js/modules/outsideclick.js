8bfdf119fba02b92ae5aa444c42308f966b67421
William <william.teletabs@hotmail.com>
2022-11-08 15:34:54 -0300

    Limpando arquivos

--- js/modules/outsideclick.js
+++
@@ -1,9 +0,0 @@
-function outsideClick(callback) {
-  const html = document.documentElement;
-  html.addEventListener('click', handleOutsideClick);
-  function handleOutsideClick(event) {
-    if (element.contains) {
-      callback();
-    }
-  }
-}
--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initFetchBtc from './modules/fetch-btc.js';
 import initAnimaScroll from './modules/anima-scroll.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 import outsideClick from './modules/outsideclick.js';
 import initMenuMobile from './modules/menu-mobile.js';
 
@@ -9,5 +10,6 @@ initScrollSuave();
 initFetchBtc();
 initAnimaScroll();
 initFuncionamento();
+initDropdownMenu();
 outsideClick();
 initMenuMobile();
