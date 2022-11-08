30430a0532e38c5b2f4908dccb03dd0e077fec94
William <william.teletabs@hotmail.com>
2022-11-08 18:13:41 -0300

    Importando arquivos

--- js/script.js
+++ js/script.js
@@ -3,13 +3,9 @@ import initFetchBtc from './modules/fetch-btc.js';
 import initAnimaScroll from './modules/anima-scroll.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
-import outsideClick from './modules/outsideclick.js';
-import initMenuMobile from './modules/menu-mobile.js';
 
 initScrollSuave();
 initFetchBtc();
 initAnimaScroll();
 initFuncionamento();
 initDropdownMenu();
-outsideClick();
-initMenuMobile();
