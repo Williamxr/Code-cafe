6bb55f40e4adc255b5b4f9702cf84c2bb96a5d95
William <william.teletabs@hotmail.com>
2022-11-21 19:27:06 -0300

    Compactando código para facilitar leitura

--- js/modules/fetch-btc.js
+++ js/modules/fetch-btc.js
@@ -1,5 +1,5 @@
 export default function initFetchBtc() {
-  fetch('https://blockchain.info/ticker').then((response) => response.json()).then((btc) => {
+  fetch('https://blockchain.info/ticker').then((r) => r.json()).then((btc) => {
     const btcPreco = document.querySelector('.btc-preco');
     btcPreco.innerText = (1000 / btc.BRL.sell).toFixed(3);
   });
